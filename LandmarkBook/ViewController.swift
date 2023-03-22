a8a3106478db3d76b86ac0fa858040d7a3d9c8a3
engineerStuardo <engeniero.stuardo@gmail.com>
2023-03-22 12:22:36 -0600

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Italo Stuardo on 22/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
